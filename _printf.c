9f93fe82a399c262f5409eec9d0dfa6c556d7846
dammyaking <damola.akintola@gmail.com>
2023-04-25 12:35:03 -0700

    code debug

--- _printf.c
+++ _printf.c
@@ -3,8 +3,8 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf -The printf function
- * @format: set format.
+ * _printf - Printf function
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -53,7 +53,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if there is any
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
